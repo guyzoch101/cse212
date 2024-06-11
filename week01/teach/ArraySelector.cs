71efc75e173a4ab36f5c3556ab09af2bdb614a4c
guyzoch101 <tristanmanhei@gmail.com>
2024-06-10 18:07:48 -0600

    Problem 2

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,23 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        int selectIndex;
+        int list1Index = 0;
+        int list2Index = 0;
+        int[] newArray = new int[select.Length];
+
+        for (selectIndex = 0; selectIndex <= select.Length - 1; ++selectIndex) {
+            if (select[selectIndex] == 1) {
+                newArray[selectIndex] = list1[list1Index];
+
+                ++list1Index;
+            }
+            else if (select[selectIndex] == 2) {
+                newArray[selectIndex] = list2[list2Index];
+
+                ++list2Index;
+            }
+        }
+        return newArray;
     }
 }
