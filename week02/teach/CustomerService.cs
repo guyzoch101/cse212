751335bd22a708400d24bdf206c033c459dbaaab
guyzoch101 <tristanmanhei@gmail.com>
2024-06-19 16:20:02 -0600

    5 tests

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -11,24 +11,74 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: 1 customer
+        // Expected Result: 1 customer added to the queue
         Console.WriteLine("Test 1");
+        var test1 = new CustomerService(10);
+        test1.AddNewCustomer();
+        Console.WriteLine($"Before serving: {test1}");
+        test1.ServeCustomer();
+        Console.WriteLine($"After serving: {test1}");
 
-        // Defect(s) Found: 
+        // Defect(s) Found: should save customer in variable before removing from Queue
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: 2 customers
+        // Expected Result: 2 customers added to the queue
         Console.WriteLine("Test 2");
+        var test2 = new CustomerService(10);
+        test2.AddNewCustomer();
+        test2.AddNewCustomer();
+        Console.WriteLine($"Before serving: {test2}");
+        test2.ServeCustomer();
+        test2.ServeCustomer();
+        Console.WriteLine($"After serving: {test2}");
 
-        // Defect(s) Found: 
+        // Defect(s) Found: none
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+        // Test 3
+        // Scenario: no customers
+        // Expected Result: "No customers in Queue" displayed
+        Console.WriteLine("Test 3");
+        var test3 = new CustomerService(10);
+        Console.WriteLine($"Before serving: {test3}");
+        test3.ServeCustomer();
+        Console.WriteLine($"After serving: {test3}");
+
+        // Defect(s) Found: none
+
+        Console.WriteLine("=================");
+
+        // Test 4
+        // Scenario: enforced maximum customers
+        // Expected Result: Maximum customers = 10 when an invalid number is entered
+        Console.WriteLine("Test 4");
+        var test4 = new CustomerService(-1);
+        Console.WriteLine(test4);
+
+        // Defect(s) Found: none
+
+        Console.WriteLine("=================");
+
+        // Test 5
+        // Scenario: overflowing number of customers
+        // Expected Result: "Maximum customers in Queue" is displayed while exceeding maximum customer
+        Console.WriteLine("Test 5");
+        var test5 = new CustomerService(3);
+        for(int i = 1; i <= (3 + 1); ++i) {
+            test5.AddNewCustomer();
+            // Console.WriteLine(test5);
+        }
+
+        // Defect(s) Found: _queue.Count >(=) _maxSize -> only ">" allows the queue to overflow by 1
+        // error message only appears when _queue.Count > _maxSize, which is after the Queue is overflew
+
+        Console.WriteLine("=================");
     }
 
     private readonly List<Customer> _queue = new();
@@ -67,7 +117,7 @@ public class CustomerService {
     /// </summary>
     private void AddNewCustomer() {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -88,8 +138,12 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
+        if (_queue.Count == 0) {
+            Console.WriteLine("No customers in Queue.");
+            return;
+        }
+        var customer = _queue[0]; // save customer first before removing
         _queue.RemoveAt(0);
-        var customer = _queue[0];
         Console.WriteLine(customer);
     }
 
