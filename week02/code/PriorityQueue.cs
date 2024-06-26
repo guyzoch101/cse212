731ee9f7366b7d09e1616fd9a4b7c987dcaeee4d
guyzoch101 <tristanmanhei@gmail.com>
2024-06-26 01:05:08 -0600

    PriorityQueue bug fixed

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -21,7 +21,7 @@
         }
 
         // Find the index of the item with the highest priority to remove
-        var highPriorityIndex = 0;
+        int highPriorityIndex = 0;
         for (int index = 1; index < _queue.Count; index++) {
             if (_queue[index].Priority > _queue[highPriorityIndex].Priority) {
                 highPriorityIndex = index;
