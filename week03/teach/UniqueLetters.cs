dccc0c2e043aa3cb7a911922eaba168f43da21e9
guyzoch101 <tristanmanhei@gmail.com>
2024-06-26 14:46:00 -0600

    Team Activity Step 4

--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -15,11 +15,21 @@
     /// <returns>true if all letters are unique, otherwise false</returns>
     private static bool AreUniqueLetters(string text) {
         // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
-        for (var i = 0; i < text.Length; ++i) {
-            for (var j = 0; j < text.Length; ++j) {
-                // Don't want to compare to yourself ... that will always result in a match
-                if (i != j && text[i] == text[j])
-                    return false;
+        // for (var i = 0; i < text.Length; ++i) {
+        //     for (var j = 0; j < text.Length; ++j) {
+        //         // Don't want to compare to yourself ... that will always result in a match
+        //         if (i != j && text[i] == text[j])
+        //             return false;
+        //     }
+        // }
+
+        var uniqueCharacters = new HashSet<char>();
+        foreach (char c in text) {
+            if (uniqueCharacters.Contains(c)) {
+                return false;
+            }
+            else {
+                uniqueCharacters.Add(c);
             }
         }
 
